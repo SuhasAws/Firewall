80c47b77a56eb72fb4aee5232056cbb90920e869
SuhasAws <125847984+SuhasAws@users.noreply.github.com>
2023-08-13 20:25:52 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -224,6 +224,257 @@ variable "APGW-BPRR-priority" {
     type = number
     description = "priority of backend request routing rule"
 }
+
+variable "firewall_policy_name" {
+
+    description = "name of your firewall policy"
+
+    type = string
+
+}
+
+variable "firewall_policy_rule_collection_group_name" {
+
+    description = "name of your firewall policy rule collection group."
+
+    type = string
+
+}
+
+variable "firewall_policy_rule_collection_group_priority" {
+
+    description = "priority"
+
+    type = number
+
+}
+
+variable "app_rule_collection_name" {
+
+    description = "name of your app rule collection"
+
+    type = string
+
+}
+
+variable "app_rule_collection_priority" {
+
+    description = "priority"
+
+    type = number
+
+}
+
+variable "app_rule_collection_action" {
+
+    description = "action"
+
+    type = string
+
+}
+
+variable "app_rule_collection_rule1" {
+
+    description = "app  rule collection rule 1"
+
+    type = string
+
+}
+
+variable "app_rule_collection_rule1_protocol1" {
+
+    description="app_rule_collection_rule1_protocol1"
+
+    type = string
+
+}
+
+variable "app_rule_collection_rule1_port1" {
+
+    description = "app_rule_collection_rule1_port1"
+
+    type = number
+
+}
+
+variable "app_rule_collection_rule1_protocol2" {
+
+    description = "app_rule_collection_rule1_protocol2"
+
+    type = string
+
+}
+
+variable "app_rule_collection_rule1_port2" {
+
+    description = "app_rule_collection_rule1_port2" 
+
+    type = number
+
+}
+
+variable "app_rule_collection_source_addresses" {
+
+    description = "app_rule_collection_source_addresses"
+
+    type = list(string)
+
+}
+
+variable "app_rule_collection_destination_fqdns" {
+
+    description = "app_rule_collection_destination_fqdns"
+
+    type = list(string)
+
+}
+
+variable "network_rule_collection_name" {
+
+    description = "network_rule_collection_name"
+
+    type = string
+
+}
+
+variable "network_rule_collection_priority" {
+
+    description = "network_rule_collection_priority"
+
+    type = number
+
+}
+
+variable "network_rule_collection_action" {
+
+    description = "network_rule_collection_action"
+
+    type = string
+
+}
+
+variable "network_rule_collection_rule1_name" {
+
+description = "network_rule_collection_rule1.name"
+
+type = string
+
+}
+
+variable "network_rule_collection_rule1_protocols" {
+
+    description = "network_rule_collection_rule1.protocols"
+
+    type = list(string)
+
+}
+
+variable "network_rule_collection_rule1_source_addresses" {
+
+description = "network_rule_collection_rule1.source_addresses"
+
+type = list(string)
+
+}
+
+variable "network_rule_collection_rule1_destination_addresses" {
+
+    description = "network_rule_collection_rule1.destination_addresses"
+
+    type = list(string)
+
+}
+
+variable "network_rule_collection_rule1_destination_ports" {
+
+    description = "network_rule_collection_rule1.destination_ports"
+
+    type = list(string)
+
+}
+
+variable "nat_rule_collection_rule_name" {
+
+    description = "nat_rule_collection_rule1_name"
+
+    type = string
+
+}
+
+variable "nat_rule_collection_rule_priority" {
+
+    description = "nat_rule_collection_rule1_name"
+
+    type = number
+
+}
+
+variable "nat_rule_collection_rule_action" {
+
+    description = "nat_rule_collection_rule1_name"
+
+    type = string
+
+}
+
+variable "nat_rule_collection_rule1_name" {
+
+    description = "nat_rule_collection_rule1_name"
+
+    type = string
+
+}
+
+variable "nat_rule_collection_rule1_protocols" {
+
+    description = "nat_rule_collection_rule1_protocols"
+
+    type = list(string)
+
+}
+
+variable "nat_rule_collection_rule1_source_addresses" {
+
+    description = "nat_rule_collection_rule1_source_addresses"
+
+    type = list(string)
+
+}
+
+variable "nat_rule_collection_rule1_destination_address" {
+
+    description = "nat_rule_collection_rule1_destination_address"
+
+    type = string
+
+}
+
+variable "nat_rule_collection_rule1_destination_ports" {
+
+    description = "nat_rule_collection_rule1_destination_ports"
+
+    type = list(string)
+
+}
+
+variable "nat_rule_collection_rule1_translated_address" {
+
+    description = "nat_rule_collection_rule1_translated_address"
+
+    type = string
+
+}
+
+variable "nat_rule_collection_rule1_translated_port" {
+
+    description = "nat_rule_collection_rule1_translated_port"
+
+    type = string
+
+}
+
+
+
 variable "BSTN-SB1-name" {
     type = string
     description = "Name of Bastion Subnet"
